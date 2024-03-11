217cd22000e2c127d8e10261d3a0ac4dc66a6008
Johnathan Walker <91965220+johnnywalker-git@users.noreply.github.com>
2024-03-11 21:18:07 +0000

    started projects page

---
+++ src/assets/components/ProjectWrapper.tsx
@@ -0,0 +1,8 @@
+const ProjectWrapper = () => {
+    return(
+    <div className="w-2/5 bg-blue-600">
+        <p>hello</p>
+    </div>)
+}
+
+export default ProjectWrapper;
--- src/assets/components/Projects.tsx
+++ src/assets/components/Projects.tsx
@@ -1,3 +1,5 @@
+import ProjectWrapper from "./ProjectWrapper"
+
 const Projects = () => {
    return<>
    <div className="ml-auto mt-auto flex flex-col lg:w-2/3 bg-white items-center min-h-screen">
@@ -5,6 +7,7 @@ const Projects = () => {
         <h2 className="text-white text-2xl m-3 bg-gray-800 bg-opacity-60 p-1 font-title">Projects</h2>
         <h3 className="text-white text-l text-center bg-black bg-opacity-60 p-1 font-text">My portfolio is always expanding allowing<br /> me to utilise my skills in real-world applications</h3>
         </div>
+    <ProjectWrapper />
     </div>
     <div>
     </div>
